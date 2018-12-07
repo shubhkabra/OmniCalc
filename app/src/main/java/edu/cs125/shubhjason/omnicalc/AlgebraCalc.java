43cdd6f3661d664b77df01538ccd61454872cd41
shubh <shubhankarkabra@outlook.com>
2018-12-06 23:15:41 -0600

    Math Parser?

--- app/src/main/java/edu/cs125/shubhjason/omnicalc/AlgebraCalc.java
+++ app/src/main/java/edu/cs125/shubhjason/omnicalc/AlgebraCalc.java
@@ -1,13 +1,16 @@
 package edu.cs125.shubhjason.omnicalc;
 
+import android.arch.core.util.Function;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import org.mariuszgromada.math.mxparser.*;
+
+import javax.xml.xpath.XPathExpression;
 
 public class AlgebraCalc extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
