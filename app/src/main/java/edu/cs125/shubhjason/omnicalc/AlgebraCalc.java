10cdf009ebad122a8d47915536916590886696d5
shubh <shubhankarkabra@outlook.com>
2018-12-08 00:46:49 -0600

    Removed the stupid module

--- app/src/main/java/edu/cs125/shubhjason/omnicalc/AlgebraCalc.java
+++ app/src/main/java/edu/cs125/shubhjason/omnicalc/AlgebraCalc.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import org.mariuszgromada.math.mxparser.*;
 
 import javax.xml.xpath.XPathExpression;
 
