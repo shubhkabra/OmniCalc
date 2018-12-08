aa90072759ee0af443650a7a6c5257cfaaf45d96
Jason Drews <jmdrews2@illinois.edu>
2018-12-08 16:44:30 -0600

    algebra test

--- app/src/main/java/edu/cs125/shubhjason/omnicalc/AlgebraStuff.java
+++ app/src/main/java/edu/cs125/shubhjason/omnicalc/AlgebraStuff.java
@@ -1,4 +1,18 @@
 package edu.cs125.shubhjason.omnicalc;
 
+import org.mariuszgromada.math.mxparser.Argument;
+import org.mariuszgromada.math.mxparser.Expression;
+import org.mariuszgromada.math.mxparser.mathcollection.Calculus;
+
 public class AlgebraStuff {
+    public static double[] solve(String[] sides) {
+        if (sides == null || sides.length < 2) {
+            return null;
+        }
+        double[] solns = new double[5];
+        Expression expr = new Expression(sides[0] + " - (" + sides[1] + ")");
+        Argument x = new Argument("x");
+        solns[0] = Calculus.solveBrent(expr, x, -10, 10, .0001, 100);
+        return solns;
+    }
 }
