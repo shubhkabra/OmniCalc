cd33592e66f1266c9ae0f86dec8b472be2b7be21
shubh <shubhankarkabra@outlook.com>
2018-12-08 15:23:13 -0600

    Added voice commands to everything

--- app/src/main/java/edu/cs125/shubhjason/omnicalc/AlgebraCalc.java
+++ app/src/main/java/edu/cs125/shubhjason/omnicalc/AlgebraCalc.java
@@ -1,15 +1,24 @@
 package edu.cs125.shubhjason.omnicalc;
 
 import android.arch.core.util.Function;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.speech.RecognizerIntent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Locale;
 
 import javax.xml.xpath.XPathExpression;
 
 public class AlgebraCalc extends AppCompatActivity {
+    private final int Reqcodespeechinput = 100;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,9 +28,39 @@ public class AlgebraCalc extends AppCompatActivity {
         voiceButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                final TextView answerThing = findViewById(R.id.textView3);
-                answerThing.setText("You clicked!");
+                promptspeechinput();
             }
         });
+    }
+    private void promptspeechinput() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
+                getString(R.string.speech_prompt));
+        try {
+            startActivityForResult(intent, Reqcodespeechinput);
+        } catch (ActivityNotFoundException a) {
+            Toast.makeText(getApplicationContext(), getString(R.string.speech_not_supported), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case Reqcodespeechinput: {
+                if (resultCode == RESULT_OK && null != data) {
+
+                    ArrayList<String> result = data
+                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    final EditText answerThing = findViewById(R.id.edittext);
+                    answerThing.setText(result.get(0));
+                    break;
+                }
+            }
+
+        }
     }
 }
