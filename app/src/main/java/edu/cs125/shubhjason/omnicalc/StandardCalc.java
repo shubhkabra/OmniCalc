957fb17bb6e88111f565f19be9172e4371041f43
shubh <shubhankarkabra@outlook.com>
2018-12-08 14:54:57 -0600

    Removed MXParser, No errors :)

--- app/src/main/java/edu/cs125/shubhjason/omnicalc/StandardCalc.java
+++ app/src/main/java/edu/cs125/shubhjason/omnicalc/StandardCalc.java
@@ -9,8 +9,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-import org.mariuszgromada.math.mxparser.*;
-import org.mariuszgromada.math.mxparser.mathcollection.Calculus;
 
 
 public class StandardCalc extends AppCompatActivity {
